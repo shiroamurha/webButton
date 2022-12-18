80cd17ea204421b38b194de0440ba47e5be88d99
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-12-18 18:22:59 -0300

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -55,3 +55,19 @@ def d():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
