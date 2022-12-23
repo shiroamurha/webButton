145d74d62f3ef7fdd78227604d3536003de3a3b1
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-12-23 17:16:22 -0300

    Add files via upload

--- inspector.py
+++ inspector.py
@@ -1,4 +1,5 @@
-from playwright.sync_api import sync_playwright, Error
+from playwright.sync_api import sync_playwright
+import os
 import json 
 
 
@@ -12,10 +13,14 @@ def clear_cookies(cookies):
 
 class Inspector():
 
-    def __init__(self, new_session):
+    def __init__(self):
 
+        
+        self.commands = json.load(open('static/commands.json', 'r'))
         cookies = json.load(open('static/state.json', 'r'))
-        if new_session:
+        self.saved_output = ' '
+        self.output = ' '
+        if self.commands.get('new_session'): # key value -> true/false
             clear_cookies(cookies) # only call it if the session is new 
 
         playwright = sync_playwright().start() 
@@ -23,25 +28,41 @@ class Inspector():
         self.context = self.browser.new_context(java_script_enabled=False)
         self.context.add_cookies(cookies)
         self.page = self.context.new_page()
-        self.page.goto("https://glamorously-beautiful-iris-flat-dev.wayscript.cloud/")
+
+        # sometimes playwright just cant access for no reason
+        try:
+            self.page.goto("https://glamorously-beautiful-iris-flat-dev.wayscript.cloud/")
+        except:
+            self.page.goto("https://glamorously-beautiful-iris-flat-dev.wayscript.cloud/")
 
     def check_output(self):
         #print(page.content())
         self.page.reload()
-        output = self.page.locator('textarea[id="output"]').text_content()
-        return output
+        temp_output = self.page.locator('textarea[id="output"]').text_content()
+        
+        if self.saved_output != temp_output:
+            self.output = temp_output 
+
+            for command in self.commands.get(self.output): # for each command line inside the list at key on commands dict  
+                os.system(command)
+
+            # assert self.saved_output == temp_output
+        return self.output
         
 
     def loop(self):
         
         try:
-            print(self.check_output())
-            self.page.wait_for_timeout(100)
+            self.saved_output = self.check_output()
+            # self.page.wait_for_timeout(100)
             self.loop() 
 
         except KeyboardInterrupt:
-            raise KeyboardInterrupt
-            self.close()
+            raise '' 
+            try:
+                self.close()
+            except:
+                pass
         
     def close(self):# aperta agora
         self.context.close()
@@ -50,5 +71,5 @@ class Inspector():
 
 
 if __name__ == "__main__":
-    Inspector(new_session=False).loop()
+    Inspector().loop()
 
